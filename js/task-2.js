1447c4f46be52ef04331c75e8dbcf75f6deaebfa
VsemDobra <ivanpampuha88@gmail.com>
2025-06-29 11:28:12 +0300

    Bug fixing

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,9 @@
-const getUsersWithFriend = (users, friendName) =>
-    users.filter(user => user.friends.includes(friendName));
-  
+
+const getUsersWithFriend = (users, friendName) => {
+  if (!friendName) return [];
+  return users.filter(user => user.friends.includes(friendName));
+};
+
   const allUsers = [
     {
       name: "Moore Hensley",
@@ -33,28 +36,9 @@ const getUsersWithFriend = (users, friendName) =>
   ];
   
   console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-  // [
-  //   {
-  //     name: "Sharlene Bush",
-  //     friends: ["Briana Decker", "Sharron Pace"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
-  
-  console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-  // [
-  //   {
-  //     name: "Elma Head",
-  //     friends: ["Goldie Gentry", "Aisha Tran"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
-  
-console.log(getUsersWithFriend(allUsers));
+
+  console.log(getUsersWithFriend(allUsers, "Goldie Gentry")); 
+
+  console.log(getUsersWithFriend(allUsers)); 
+
   
